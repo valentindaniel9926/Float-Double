bff2abf5fd32610f4f0c1b10154f39455580d945
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2021-04-12 08:21:41 +0300

    Add files via upload

---
+++ Float&Double/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double myDoubleMaxVAlue = Double.MAX_VALUE;
+        double myDoubleMinVAlue = Double.MIN_VALUE;
+        System.out.println("THE MAXIMUM  VALUE FOR DOUBLE  IS : "  + myDoubleMaxVAlue);
+        System.out.println("THE Minimum VALUE FOR DOUBLE  IS : "  + myDoubleMinVAlue);
+
+        Float myFloatMaxVAlue = Float.MAX_VALUE;
+        Float myFloatMinVAlue = Float.MIN_VALUE;
+        System.out.println("THE MAXIMUM  VALUE FOR Float  IS : "  + myFloatMaxVAlue);
+        System.out.println("THE Minimum VALUE FOR Float  IS : "  + myFloatMinVAlue);
+        int myIntvalue=5/3;
+        float myFloatValue =5f/3f;
+        double myDoubleValue =5f/3d;
+        System.out.println("MyIntValue = "  + myIntvalue);
+        System.out.println("MyFloatValue = "  + myFloatValue);
+        System.out.println("MyDoubleValue  " + myDoubleValue);
+
+
+    }
+    }
+
